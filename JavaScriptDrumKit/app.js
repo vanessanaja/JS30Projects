0fb459fccfd5511bae5ae8426c963f4d9d9685d1
vanessanaja <vanessacnaja@gmail.com>
2018-12-15 10:02:44 -0800

    add key event listeners

--- JavaScriptDrumKit/app.js
+++ JavaScriptDrumKit/app.js
@@ -1,5 +1,13 @@
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     if(!audio) return;
+    audio.currentTime = 0; //will rewind to beginning each time it's played
     audio.play();
+    key.classList.add('playing');
 });
+
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
