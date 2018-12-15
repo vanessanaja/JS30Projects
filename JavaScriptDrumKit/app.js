ef8ebd8c216dacd2c966d1c5c5d5de36a4e55167
vanessanaja <vanessacnaja@gmail.com>
2018-12-15 10:37:04 -0800

    Refactor so it works
    Wes' solution didn't actually work. Something must have changed since he made this video so I changed how to remove the transition from the key.

--- JavaScriptDrumKit/app.js
+++ JavaScriptDrumKit/app.js
@@ -1,13 +1,20 @@
-window.addEventListener('keydown', function(e){
+function removeTransition(e) {
+    // if(e.propertyName !== 'transform') return;
+    // e.target.classList.remove('playing');
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+    key.classList.remove('playing');
+}
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
-    if(!audio) return;
-    audio.currentTime = 0; //will rewind to beginning each time it's played
-    audio.play();
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+    if (!audio) return;
     key.classList.add('playing');
-});
+    audio.currentTime = 0; //will rewind to beginning each time it's played
+    audio.play();   
+}
 
 
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = Array.from(document.querySelectorAll('.key'));
+window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', removeTransition);
