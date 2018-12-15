e64ce26554ae81d051fb2aa4765841e7a3e694f0
vanessanaja <vanessacnaja@gmail.com>
2018-12-15 10:38:08 -0800

    refactor js order

--- JavaScriptDrumKit/app.js
+++ JavaScriptDrumKit/app.js
@@ -1,10 +1,3 @@
-function removeTransition(e) {
-    // if(e.propertyName !== 'transform') return;
-    // e.target.classList.remove('playing');
-    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
-    key.classList.remove('playing');
-}
-
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
     const key = document.querySelector(`div[data-key='${e.keyCode}']`);
@@ -12,6 +5,13 @@ function playSound(e) {
     key.classList.add('playing');
     audio.currentTime = 0; //will rewind to beginning each time it's played
     audio.play();   
+}
+
+function removeTransition(e) {
+    // if(e.propertyName !== 'transform') return;
+    // e.target.classList.remove('playing');
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+    key.classList.remove('playing');
 }
 
 
