46f14ce0f3fab5b9794204f593180969577517f7
vanessanaja <vanessacnaja@gmail.com>
2018-12-16 11:30:00 -0800

    delete console.log

--- CSSVariables/app.js
+++ CSSVariables/app.js
@@ -2,7 +2,6 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate(){
     const suffix = this.dataset.sizing || '';
-    console.log(this.name);
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
