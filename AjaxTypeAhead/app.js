f617b3fb71271d5d74be0b30fee334e834e13f59
vanessanaja <vanessacnaja@gmail.com>
2018-12-19 11:55:33 -0800

    add function findMatches

--- AjaxTypeAhead/app.js
+++ AjaxTypeAhead/app.js
@@ -5,3 +5,11 @@ const cities = [];
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data))
+
+    function findMatches(wordToMatch, cities){
+        return cities.filter(place => {
+            //figure out if city or state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi')
+            return place.city.match(regex) || place.state.match(regex);
+        });
+    }
