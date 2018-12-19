5ff8414b269f4ad3c24a199fa20ea8bc659545ff
vanessanaja <vanessacnaja@gmail.com>
2018-12-19 12:07:56 -0800

    add displayMatches() and querySelectors

--- AjaxTypeAhead/app.js
+++ AjaxTypeAhead/app.js
@@ -4,12 +4,24 @@ const cities = [];
 
 fetch(endpoint)
     .then(blob => blob.json())
-    .then(data => cities.push(...data))
+    .then(data => cities.push(...data));
 
     function findMatches(wordToMatch, cities){
         return cities.filter(place => {
             //figure out if city or state matches what was searched
-            const regex = new RegExp(wordToMatch, 'gi')
+            const regex = new RegExp(wordToMatch, 'gi');
             return place.city.match(regex) || place.state.match(regex);
         });
     }
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, cities);
+    console.log(matchArray);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
