97aa6b4d36836b062904bd958824d2227e81dc32
vanessanaja <vanessacnaja@gmail.com>
2018-12-20 10:14:48 -0800

    refactor comments.find

--- ArrayCardioDay2/app.js
+++ ArrayCardioDay2/app.js
@@ -43,11 +43,17 @@ const comments = [
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
 
-    const comment = comments.find(function(comment){
-      if(comment.id === 823423){
-        return true;
-      }
-    });
+    // const comment = comments.find(function(comment){
+    //   if(comment.id === 823423){
+    //     return true;
+    //   }
+    // });
+
+    // console.log(comment);
+
+    const comment = comments.find(comment => 
+      comment.id === 823423
+   );
 
     console.log(comment);
 
