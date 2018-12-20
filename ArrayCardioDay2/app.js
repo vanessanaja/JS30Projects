e13e76cfa159c9b0ffa2e90de4e7cfc78d90f8cd
vanessanaja <vanessacnaja@gmail.com>
2018-12-20 10:11:11 -0800

    add comments.find

--- ArrayCardioDay2/app.js
+++ ArrayCardioDay2/app.js
@@ -43,6 +43,14 @@ const comments = [
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
 
+    const comment = comments.find(function(comment){
+      if(comment.id === 823423){
+        return true;
+      }
+    });
+
+    console.log(comment);
+
     // Array.prototype.findIndex()
     // Find the comment with this ID
     // delete the comment with the ID of 823423
