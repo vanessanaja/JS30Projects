dd1f801425982089dec1b32431912bc416675d06
vanessanaja <vanessacnaja@gmail.com>
2018-12-21 09:01:09 -0800

    add set up function draw

--- canvas/app.js
+++ canvas/app.js
@@ -7,9 +7,18 @@ ctx.strokeStyle = '#BAD55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
-//only draw when mouseclick is down
+//only draw when mousedown
 let isDrawing = false; 
 let lastX = 0;
 let lastY = 0;
 
+function draw(e) {
+  if(!isDrawing) return; //stop function from running when not mousedown
+  console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
 
