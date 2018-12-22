f02d5fc6671832a81f5224d2663feefb56e5da34
vanessanaja <vanessacnaja@gmail.com>
2018-12-22 08:00:21 -0800

    initial commit

---
+++ DevToolsFun/app.js
@@ -0,0 +1,7 @@
+const dogs = [{name: "Tiny", age: 18}, {name: 'Bugsy', age: 8}];
+
+function makeTurquoise(){
+  const p = document.querySelector('p');
+  p.style.color = '#62f7dc';
+  p.style.fontSize = '55px';
+}
