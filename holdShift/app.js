aaee8340ec016517024f182f13e4aaabff9bd3c3
vanessanaja <vanessacnaja@gmail.com>
2018-12-23 11:07:57 -0800

    add logic

--- holdShift/app.js
+++ holdShift/app.js
@@ -1,8 +1,24 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-function handleCheck(e){
-  console.log(e);
-};
-
 checkboxes.forEach(checkbox => checkbox.addEventListener(
   'click', handleCheck));
+
+let lastChecked
+
+function handleCheck(e){
+    //check if they had shift key down and are checking the box
+    let inBetween = false;
+    if(e.shiftKey && this.checked)  {
+      checkboxes.forEach(checkbox => {
+        console.log(checkbox);
+        if(checkbox === this || checkbox === lastChecked){
+          inBetween = !inBetween;
+        }
+
+        if(inBetween){
+          checkbox.checked = true;
+        }
+      });
+    }
+    lastChecked = this;
+  }
