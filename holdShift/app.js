1702e55b80d7c27a25eaec0548d29e78bd4bdb1d
vanessanaja <vanessacnaja@gmail.com>
2018-12-24 11:12:11 -0800

    video player initial commit

--- holdShift/app.js
+++ holdShift/app.js
@@ -3,18 +3,18 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 checkboxes.forEach(checkbox => checkbox.addEventListener(
   'click', handleCheck));
 
-let lastChecked
+let lastChecked;
 
 function handleCheck(e){
     //check if they had shift key down and are checking the box
     let inBetween = false;
-    if(e.shiftKey && this.checked)  {
+    if(e.shiftKey && []
+    }"giuy
+    \this.checked)  {
       checkboxes.forEach(checkbox => {
-        console.log(checkbox);
         if(checkbox === this || checkbox === lastChecked){
           inBetween = !inBetween;
         }
-
         if(inBetween){
           checkbox.checked = true;
         }
