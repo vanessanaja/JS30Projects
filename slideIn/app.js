c0e39b17c6f9ad555c4a20eb29c27cab759ba654
vanessanaja <vanessacnaja@gmail.com>
2018-12-27 09:03:36 -0800

    add checkSlide function

--- slideIn/app.js
+++ slideIn/app.js
@@ -13,3 +13,22 @@ function debounce(func, wait = 20, immediate = true) {
   };
 }
 
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(slideImage => {
+    // halfway through image
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    // bottom of image
+    const imageBottom = (slideImage.offsetTop + slideImage.height);
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if(isHalfShown && isNotScrolledPast){
+      slideImage.classList.add('active');
+    } else {
+      slideImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
