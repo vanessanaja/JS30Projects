3bb2af573ed319b8984b4cbf36349632da6387c4
vanessanaja <vanessacnaja@gmail.com>
2018-12-29 15:53:46 -0800

    add function addItem

--- localStorageExercise/app.js
+++ localStorageExercise/app.js
@@ -1,3 +1,18 @@
-const addItems = document.querySelector('add-items');
+const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text: text,
+    //or you can use ES6 shorthand property which is just text,
+    done: false
+  }
+  // clear input
+  this.reset(); 
+  console.log(item);
+}
+
+addItems.addEventListener('submit', addItem);
