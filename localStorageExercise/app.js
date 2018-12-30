6e7d8d1b7665f2b43fe80823ff0c7c5bfcb56004
vanessanaja <vanessacnaja@gmail.com>
2018-12-29 16:23:03 -0800

    update populateLIST add localStorage

--- localStorageExercise/app.js
+++ localStorageExercise/app.js
@@ -1,28 +1,32 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
   const text = (this.querySelector('[name=item]')).value;
   const item = {
-    text: text,
-    //or you can use ES6 shorthand property which is just text,
+    text,
     done: false
-  }
+  };
+
   items.push(item);
-  // clear input
-  this.reset(); 
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
 }
 
 function populateList(plates = [], platesList) {
-  platesList.innerHTMl =  plates.map((plate, i) => {
+  platesList.innerHTML = plates.map((plate, i) => {
     return `
       <li>
-        <label>${plate.text}</label>      
-      </li>    
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
     `;
   }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList);
