0ece91bf46c412a164ca8e01230e50c5a9a24da9
vanessanaja <vanessacnaja@gmail.com>
2019-01-04 10:20:29 -0800

    include native speach on DOM

--- nativeSpeechRecognition/app.js
+++ nativeSpeechRecognition/app.js
@@ -8,7 +8,18 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-  console.log(e);
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal){
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+    console.log(transcript);
 });
 
+recognition.addEventListener('end', recognition.start);
 recognition.start();
