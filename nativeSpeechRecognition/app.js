8517f36577e0e37bc1511b13cf5d461709def4b1
vanessanaja <vanessacnaja@gmail.com>
2019-01-04 09:53:28 -0800

    Update nativeSpeechRecognition/app.js
    connect speech recognition

--- nativeSpeechRecognition/app.js
+++ nativeSpeechRecognition/app.js
@@ -2,3 +2,13 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  console.log(e);
+});
+
+recognition.start();
