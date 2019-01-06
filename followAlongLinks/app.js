84a0fe4de5740dc058f5d00ea28b168d4b3a254a
vanessanaja <vanessacnaja@gmail.com>
2019-01-06 09:46:13 -0800

    add coors object & update to style transform

--- followAlongLinks/app.js
+++ followAlongLinks/app.js
@@ -6,9 +6,17 @@ document.body.append(highlight);
 function highlightLink (){
   const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords);
-  highlight.style.width = `${linkCoords.width}px`;
-  highlight.style.height = `${linkCoords.height}px`;
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(a => 
