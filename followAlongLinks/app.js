07a7d1996e8e5163327aafc370c812ec0a54336b
vanessanaja <vanessacnaja@gmail.com>
2019-01-06 09:39:04 -0800

    add highlight style transform

--- followAlongLinks/app.js
+++ followAlongLinks/app.js
@@ -6,6 +6,9 @@ document.body.append(highlight);
 function highlightLink (){
   const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach(a => 
