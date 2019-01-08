85422769655eabd9d4e8d70014fd300020d1e688
vanessanaja <vanessacnaja@gmail.com>
2019-01-08 09:39:48 -0800

    update fixNav() and add style changes

--- stickyNav/app.js
+++ stickyNav/app.js
@@ -3,8 +3,10 @@ const topOfNavBar = nav.offsetTop;
 
 function fixnav(){
   if(window.scrollY >= topOfNavBar){
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
+    document.body.style.paddingTop = 0;
     document.body.classList.remove('fixed-nav');
   }
 }
