6ca74c5a122f37f18ccd80f77e5074d67121fe1a
vanessanaja <vanessacnaja@gmail.com>
2019-01-09 09:05:46 -0800

    add stopPropagation()
    and set capture back to false so we can see how stopPropagation works.

--- eventPropogation/app.js
+++ eventPropogation/app.js
@@ -2,11 +2,11 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  console.log(this);
+  e.stopPropagation();
 }
 
 document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText, {
-  capture: true
+  capture: false
 }));
