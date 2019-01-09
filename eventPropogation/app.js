918384e09e6e231e4d2d7f40c9475bb4a0797805
vanessanaja <vanessacnaja@gmail.com>
2019-01-09 08:54:04 -0800

    add event listener and callback

---
+++ eventPropogation/app.js
@@ -0,0 +1,10 @@
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+  console.log(this.classList.value);
+  console.log(this);
+}
+
+document.body.addEventListener('click', logText);
+
+divs.forEach(div => div.addEventListener('click', logText));
