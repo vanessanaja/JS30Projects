323f1cf2e5ecb694c62f8f4d1f34fc5fc1b066d7
vanessanaja <vanessacnaja@gmail.com>
2019-01-10 08:31:13 -0800

    update coords, handleEnter and handleLeave

--- dropDown/app.js
+++ dropDown/app.js
@@ -4,7 +4,12 @@ const nav = document.querySelector('.top');
 
 function handleEnter(){
   this.classList.add('trigger-enter');
-  setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  setTimeout(() => {
+    if(this.classList.contains('trigger-enter')){
+      this.classList.add('trigger-enter-active')
+      }
+  }, 150);
+  
   background.classList.add('open');
 
   const dropdown = this.querySelector('.dropdown');
@@ -13,15 +18,17 @@ function handleEnter(){
 
   const coords = {
     height: dropdownCoords.height,
-    width: dropdownCoords.width
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
   };
   background.style.setProperty('width', `${coords.width}px`);
   background.style.setProperty('height', `${coords.heights}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 }
 
 function handleLeave(){
-  this.classList.remove('trigger-enter');
-  setTimeout(() => this.classList.remove('trigger-enter-active'), 150);
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
   background.classList.remove('open');
 }
 
