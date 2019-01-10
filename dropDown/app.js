3b5955f52d8cfd6cfa7d5d43d6842795f9e4ad21
vanessanaja <vanessacnaja@gmail.com>
2019-01-10 08:14:10 -0800

    add and remove background classList

--- dropDown/app.js
+++ dropDown/app.js
@@ -5,11 +5,15 @@ const nav = document.querySelector('.top');
 function handleEnter(){
   this.classList.add('trigger-enter');
   setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
+
+  
 }
 
 function handleLeave(){
   this.classList.remove('trigger-enter');
   setTimeout(() => this.classList.remove('trigger-enter-active'), 150);
+  background.classList.remove('open');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
