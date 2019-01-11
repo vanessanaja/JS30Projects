f71bf501112fdf9910d2616e51967851492eeef8
vanessanaja <vanessacnaja@gmail.com>
2019-01-11 10:44:40 -0800

    set up mouseevent listeners

--- clickAndDrag/app.js
+++ clickAndDrag/app.js
@@ -1,1 +1,18 @@
 const slider = document.querySelector('.items');
+let isDown = false;
+let StartX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+  isDown = true;    
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+slider.addEventListener('mousemove', () => {
+  console.log(isDown);
+  console.log('Do work');
+});
