36c2bd5325e425600c081450a6418e1bbb85e7ad
vanessanaja <vanessacnaja@gmail.com>
2019-01-11 10:48:22 -0800

    change slider classList in functions

--- clickAndDrag/app.js
+++ clickAndDrag/app.js
@@ -5,14 +5,18 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', () => {
   isDown = true;    
+  slider.classList.add('active');
 });
 slider.addEventListener('mouseleave', () => {
   isDown = false;
+  slider.classList.remove('active');
 });
 slider.addEventListener('mouseup', () => {
   isDown = false;
+  slider.classList.remove('active');
 });
 slider.addEventListener('mousemove', () => {
+  if(!isDown) return;
   console.log(isDown);
   console.log('Do work');
 });
