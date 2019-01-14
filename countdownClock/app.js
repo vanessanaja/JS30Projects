2f15b7810e36073ebf1ee3e842dd29dca20b5ce1
vanessanaja <vanessacnaja@gmail.com>
2019-01-14 09:01:18 -0800

    ad let countDown to clearInterval when countdown hits 0

--- countdownClock/app.js
+++ countdownClock/app.js
@@ -1,10 +1,15 @@
+let countDown;
+
 function timer(seconds){
   const now = Date.now();
   const then = now + seconds *1000;
-  setInterval(() => {
+  countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     //check if we should stop timer
-    
+    if(secondsLeft <=0){
+      clearInterval(countDown);
+      return;
+    }
     console.log(secondsLeft);
   }, 1000);
 }
