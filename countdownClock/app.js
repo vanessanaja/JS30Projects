ba0bd07334d09a330f3a75cd0de44bb8d3f8103f
vanessanaja <vanessacnaja@gmail.com>
2019-01-14 08:58:59 -0800

    add setInterval for seconds left

--- countdownClock/app.js
+++ countdownClock/app.js
@@ -1,5 +1,10 @@
 function timer(seconds){
   const now = Date.now();
   const then = now + seconds *1000;
-  console.log({now, then});
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    //check if we should stop timer
+    
+    console.log(secondsLeft);
+  }, 1000);
 }
