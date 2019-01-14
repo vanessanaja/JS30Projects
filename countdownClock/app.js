4977685314fd4ce10ef6d5990b7329e8618d98e1
vanessanaja <vanessacnaja@gmail.com>
2019-01-14 09:07:30 -0800

    add displatyTimeLeft()

--- countdownClock/app.js
+++ countdownClock/app.js
@@ -3,13 +3,20 @@ let countDown;
 function timer(seconds){
   const now = Date.now();
   const then = now + seconds *1000;
+  displayTimeLeft(seconds);
+
   countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     //check if we should stop timer
-    if(secondsLeft <=0){
+    if(secondsLeft < 0){
       clearInterval(countDown);
       return;
     }
-    console.log(secondsLeft);
+    //display it
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds){
+  console.log(seconds);
 }
